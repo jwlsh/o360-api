0bb36c27a4328c30bd370990c848701eba0b4832
john.walsh@eaisys.com <>
2013-12-28 21:08:40 +0000

    Fixed Role class missing annotation

--- src/main/java/com/eaisys/o360/api/domain/Role.java
+++ src/main/java/com/eaisys/o360/api/domain/Role.java
@@ -1,6 +1,13 @@
 package com.eaisys.o360.api.domain;
 
-@
+import org.springframework.data.neo4j.annotation.NodeEntity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper=true)
+@NodeEntity
 public class Role extends BaseEntity {
 
   private String name;
